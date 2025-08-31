64cbacb89855522cee357b6f90a129b6a6c0720f
Ilya Mityaev <78545085+dragoryy@users.noreply.github.com>
2025-08-31 17:06:19 +0300

    add github avatar and logout (#56)

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -5,7 +5,25 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+  <details v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <summary class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-6 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.image">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </summary>
+    <ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="18" />
+          Sign out
+        </NuxtLink>
+      </li>
+    </ul>
+  </details>
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Sign In With GitHub
     <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
     <Icon v-else name="tabler:brand-github" size="24" />
